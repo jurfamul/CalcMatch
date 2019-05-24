4076f479c06ece680289c3d755fdedf4a3126ab1
Kordell <kordell.flores@du.edu>
2019-05-24 11:10:34 -0600

    Made All Cards Work on Network

--- CalcMatch/Assets/ProjectFiles/scripts/EventManager.cs
+++ CalcMatch/Assets/ProjectFiles/scripts/EventManager.cs
@@ -5,10 +5,11 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
- public class EventManager : MonoBehaviour
+ public class EventManager : Photon.PunBehaviour
 {
     public Transform spawnPoint;
     public static bool ButtOn = true;
+    //public photonButtons myButton;
     public Button myButton;
 
 
@@ -18,9 +19,12 @@ using UnityEngine.UI;
         var go = EventSystem.current.currentSelectedGameObject;
         if (go != null)
         {
-            Instantiate(Resources.Load(go.name), spawnPoint.position, spawnPoint.rotation);
+            PhotonNetwork.Instantiate(go.name, spawnPoint.position, spawnPoint.rotation,0);
+            //myButton.enabled = false;
             myButton.interactable = false;
             ButtOn = false;
+
+           
         }
         else
         {
