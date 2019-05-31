09ab9dff85c422cb1e3846cb187694610447bcba
Unknown <jurgen.famula@du.edu>
2019-05-31 11:29:04 -0600

    Removed passing owner button from server menu

---
+++ CalcMatch/Assets/ProjectFiles/scenes/scripts/Scene Changes/exit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using ExitGames.Client.Photon;
+
+public class exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void changeScene()
+    {
+        if (PhotonNetwork.isMasterClient)
+        {
+            if (PhotonNetwork.playerList.Length > 1)
+            {
+                PhotonNetwork.SetMasterClient(PhotonNetwork.playerList[1]);
+            }
+        }
+        PhotonNetwork.LeaveRoom();
+        SceneManager.LoadScene("DemoWorker-Scene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
